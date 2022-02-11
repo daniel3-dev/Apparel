1460a49a16f2329bdaab9b59247471a1cefec88b
daniel3-dev <annoyingfish2@gmail.com>
2022-02-11 01:05:00 -0600

    Finished mosaic, added overlay, added marquee text, added circle that follows cursor, started section 2

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+//Make circle follow mouse
+const circle = document.getElementsByClassName('cursor')[0];
+const body = document.getElementById('body');
+let mouseX = 0,
+  mouseY = 0;
+let xPosition = 0,
+  yPosition = 0;
+
+body.addEventListener('mousemove', function (event) {
+  getMousePos(event);
+});
+
+function getMousePos(e) {
+  mouseX = e.clientX - 40;
+  mouseY = e.clientY - 40;
+}
+
+setInterval(function () {
+  xPosition += (mouseX - xPosition) / 6;
+  yPosition += (mouseY - yPosition) / 6;
+  circle.style.left = xPosition + 'px';
+  circle.style.top = yPosition + 'px';
+}, 20);
+
 //Clears Input on page refresh
 var inputs = document.getElementsByTagName('input');
-for(i = 0; i < inputs.length; i++){
-    inputs[i].value = '';
+for (i = 0; i < inputs.length; i++) {
+  inputs[i].value = '';
 }
